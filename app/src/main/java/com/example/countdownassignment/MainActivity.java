e229643c54918276e4c28b51bada0821d3715e2a
DaRealTaylor <taylor.fontes@my.jcu.edu.au>
2022-03-15 10:05:44 +1000

    link timer to button

--- app/src/main/java/com/example/countdownassignment/MainActivity.java
+++ app/src/main/java/com/example/countdownassignment/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void startTimer(){
+    public void startTimer(){
         countDownTimer = new CountDownTimer(30000, 1000) {
             @Override
             public void onTick(long l) {
