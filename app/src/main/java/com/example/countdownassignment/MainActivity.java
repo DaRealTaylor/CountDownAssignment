744c88a0f31dc8e77afa8c8c7755bc1485a64a07
DaRealTaylor <taylor.fontes@my.jcu.edu.au>
2022-03-15 10:03:39 +1000

    create initial timer

--- app/src/main/java/com/example/countdownassignment/MainActivity.java
+++ app/src/main/java/com/example/countdownassignment/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.countdownassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
+    CountDownTimer countDownTimer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void startTimer(){
+        countDownTimer = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long l) {
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+    }
+
+    void stopTimer(){
+        if(countDownTimer != null){
+            countDownTimer.cancel();
+        }
     }
 }
