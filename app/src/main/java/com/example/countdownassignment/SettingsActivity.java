043333d3b3494179bbccbb6648ffaa957f5a8d3d
DaRealTaylor <taylor.fontes@my.jcu.edu.au>
2022-03-20 11:40:05 +1000

    create dark mode functions

--- app/src/main/java/com/example/countdownassignment/SettingsActivity.java
+++ app/src/main/java/com/example/countdownassignment/SettingsActivity.java
@@ -1,24 +1,47 @@
 package com.example.countdownassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
     public static final int SETTINGS_REQUEST = 1;
     private EditText editText;
     Button doneButton;
+    SwitchCompat switchCompat;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode() ==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.Theme_Dark);
+        } else{
+            setTheme(R.style.Theme_Light);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         doneButton = (Button)findViewById(R.id.Done);
+        switchCompat = findViewById(R.id.switch1);
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
 
     }
 
